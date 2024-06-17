d5787ad66963fec8e5f74358ce44f614177fe36f
ray hunter <ray.hunter@gmail.com>
2024-06-17 16:40:59 -0700

    github-action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+/* import { defineConfig } from 'astro/config';
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -6,4 +6,11 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   output: "server",
   adapter: cloudflare()
-});
+}); */
+
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://rayhunter.github.io',
+  base: 'drlove',
+})
